a659af204d00af4b8f612bae7cf9822eb181eed6
huisheng <huisheng@wacai.com>
2023-03-17 14:15:59 +0800

    test

--- src/api/chat_gpt.rs
+++ src/api/chat_gpt.rs
@@ -13,3 +13,31 @@ pub trait ChatApi {
         message_from_user: &str,
     ) -> Result<String>;
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::api::chat_gpt_35_turbo::ChatGpt35Turbo;
+    use crate::{settings::ChatGptConfig};
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_send_message() {
+        let api = ChatGpt35Turbo {};
+
+        let client = Client::new();
+        let config = ChatGptConfig {
+            api: "".to_owned(),
+            model: "gpt-3.5-turbo".to_owned(),
+        };
+        let context = vec![];
+        let message_from_user = "Hi, there!";
+
+        let result = api
+            .send_message(&client, &config, &context, message_from_user)
+            .await;
+
+        // Check if the result is a string
+        assert!(result.is_ok());
+    }
+}
